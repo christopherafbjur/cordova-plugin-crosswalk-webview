59df6de4ae33cfbcff52478666ceef229c8df55a
Christopher Af Bjur <chrisdevcabc@gmail.com>
2019-06-19 16:37:30 +0200

    Cordova 9.0.0 support

--- hooks/before_plugin_uninstall/000-shared_mode_special.js
+++ hooks/before_plugin_uninstall/000-shared_mode_special.js
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
 
 module.exports = function(context) {
-
     /** @external */
-    var deferral = context.requireCordovaModule('q').defer(),
-        UpdateConfig = require('./../update_config.js'),
+    var deferral = require("q").defer(),
+        UpdateConfig = require("./../update_config.js"),
         updateConfig = new UpdateConfig(context);
 
     /** Main method */
@@ -18,5 +17,4 @@ module.exports = function(context) {
     main();
 
     return deferral.promise;
-
 };
